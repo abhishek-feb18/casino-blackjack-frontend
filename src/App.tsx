5776efcf248df27ab1d242484a747a0c8ff74f12
Abhishek Sharma <abhishek@Abhisheks-MacBook-Air.local>
2023-12-29 01:20:51 -0800

    Add some routes and background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import { ROUTES } from "./utils/constants";
+import Signup from "./pages/Signup";
+import "./styles.css";
 
 function App() {
-    return <div>Hello World!</div>;
+    return (
+        <div className="page-container">
+            <BrowserRouter>
+                <Routes>
+                    <Route path={ROUTES.HOMEPAGE} element={<Homepage />} />
+                    <Route path={ROUTES.LOGIN} element={<Login />} />
+                    <Route path={ROUTES.SIGNUP} element={<Signup />} />
+                    <Route
+                        path="*"
+                        element={<Navigate to={ROUTES.HOMEPAGE} replace />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export enum ROUTES {
+    HOMEPAGE = "/",
+    LOGIN = "/login",
+    SIGNUP = "/signup",
+    SINGLE_PLAYER = "/single-player",
+    STATS_TODAY = "/stats/today",
+    STATS_LIFETIME = "/stats/all-time",
+}
